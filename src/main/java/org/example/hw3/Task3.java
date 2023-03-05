19492174c01813527391e634591f71540c25646f
udav516 <eldar.udav516@yandex.ru>
2023-03-05 19:11:09 +0300

    hw1

--- src/main/java/org/example/hw3/Task3.java
+++ src/main/java/org/example/hw3/Task3.java
@@ -1,4 +1,5 @@
 package org.example.hw3;
+
 //3.* Даны две двоичные строки a и b, вернуть их сумму в виде двоичной строки.
 //Ввод: a = "11", b = "1"
 //Вывод: "100"
@@ -6,7 +7,14 @@ package org.example.hw3;
 //Вывод: "10101"
 public class Task3 {
     public static void main(String[] args){
-
+        addBinary();
     }
-
+    public static void addBinary(){
+        String input0 = "11";
+        String input1 = "1";
+        int number0 = Integer.parseInt(input0, 2);
+        int number1 = Integer.parseInt(input1, 2);
+        int sum = number0 + number1;
+        System.out.printf(Integer.toBinaryString(sum)); //returns the answer as a binary value;
+    }
 }
