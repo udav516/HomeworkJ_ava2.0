99fb505b2f53fb4c0f0d58f71596a7d0553baf02
udav516 <eldar.udav516@yandex.ru>
2023-03-09 18:34:30 +0300

    seminar2/hw3/Task3

--- src/main/java/org/example/seminar2/hw3/Task3.java
+++ src/main/java/org/example/seminar2/hw3/Task3.java
@@ -1,8 +1,33 @@
 package org.example.seminar2.hw3;
 
 public class Task3 {
-//3) Дан массив целых чисел. Заменить отрицательные элементы на сумму индексов двузначных элементов массива.
+    //3) Дан массив целых чисел. Заменить отрицательные элементы на сумму индексов двузначных элементов массива.
     public static void main(String[] args) {
+        int[] array = new int[]{-5, 8, -2, 6, -1, 3, -7, 4, -9, 11};
+        replaceNegative(array);
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        StringBuilder res = new StringBuilder("[");
+        for (int i = 0; i < array.length; i++) {
+            res.append(array[i]).append(i == array.length - 1 ? "]" : ",");
+        }
+        System.out.println(res);
+    }
+
+    public static void replaceNegative(int[] array) {
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if ((10 <= array[i] && array[i] <= 99) || (-99 <= array[i] && array[i] <= -10)) {
+                sum += i;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) array[i] = sum;
+        }
 
     }
 }
